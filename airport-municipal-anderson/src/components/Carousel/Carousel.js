b8e74d36e784e9112b4d538164604c42b13d5d0c
gavinmcmillen <gavinmcmillen@gmail.com>
2023-11-21 15:01:47 -0500

    React-scripts downgrade due to eronous audit fix.
    Update carousel to prevent type error.

--- airport-municipal-anderson/src/components/Carousel/Carousel.js
+++ airport-municipal-anderson/src/components/Carousel/Carousel.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import classes from './Carousel.module.css';
 
-const Carousel = ({images, interval = 3000}) => {
-    const [sittingImage, setSittingImage] = useState(0);
+const Carousel = ({ images, interval = 3000 }) => {
+  const [sittingImage, setSittingImage] = useState(0);
 
-    useEffect(()=>{
-        const timer = setInterval(()=>{
-            setSittingImage = (prevIndex => prevIndex +1) % images.length
-        }, [interval]);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setSittingImage(prevIndex => (prevIndex + 1) % images.length);
+    }, interval);
+  
+    return () => clearInterval(timer);
+  }, [images.length, interval]);
 
-        return ()=> clearInterval();
-    }, [images.length.interval]);
-
-    return (
-        <div className={classes['carousel-container']}>
-            <div
+  return (
+    <div className={classes['carousel-container']}>
+      <div
         className="carousel-slide"
         style={{ transform: `translateX(-${sittingImage * 100}%)` }}
       >
@@ -22,8 +22,8 @@ const Carousel = ({images, interval = 3000}) => {
           <img key={index} src={image} alt={`Slide ${index}`} />
         ))}
       </div>
-        </div>
-    )
+    </div>
+  )
 };
 
 export default Carousel;
