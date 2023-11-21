861ab1e6b4a8c53bb988a1f0f1e1a1c44d91885f
gavinmcmillen <gavinmcmillen@gmail.com>
2023-11-21 12:05:44 -0500

    Branch broken post Bulma installation

--- airport-municipal-anderson/src/App.js
+++ airport-municipal-anderson/src/App.js
@@ -1,22 +1,26 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Carousel from './components/Carousel/Carousel';
+import ImageImports from './components/Carousel/image-import';
+//import 'bulma/css/bulma.css';
 
 function App() {
-  return (<Fragment>
-    <header>
-      <NavBar/>
-    </header>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Anderson Municipal Airport is happy to welcome you 
-        </p>
-        
+  return (
+    <Fragment>
+      <header>
+        <NavBar />
       </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Anderson Municipal Airport is happy to welcome you
+          </p>
+        </header>
+      </div>
+      <Carousel images={ImageImports} interval={3000} />
     </Fragment>
   );
 }
