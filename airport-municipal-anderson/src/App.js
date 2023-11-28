603e1f5e8234ce5a0fd4be56014b15a675db5eda
gavinmcmillen <gavinmcmillen@gmail.com>
2023-11-28 11:47:51 -0500

    Removal of Carousel as it gets off topic.
    Entire component folder deleted.
    
    Removal of componet addition to App.js

--- airport-municipal-anderson/src/App.js
+++ airport-municipal-anderson/src/App.js
@@ -1,11 +1,8 @@
 import React, { Fragment } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-//import Carousel from './components/Carousel/Carousel';
-import ImageImports from './components/Carousel/image-import';
 import 'bulma/css/bulma.css';
-import ImageCarousel from './components/Carousel/ImageCarousel';
+
 
 function App() {
   return (
@@ -13,7 +10,6 @@ function App() {
       <header>
         <NavBar />
       </header>      
-      <ImageCarousel images={ImageImports} interval={3000} />
     </Fragment>
   );
 }
