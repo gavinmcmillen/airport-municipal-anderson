0f3414b5605f417be2cc9869ae0e8abf40ea16de
gavinmcmillen <gavinmcmillen@gmail.com>
2023-12-08 10:34:31 -0500

    Update NavBarWeather.
    Update NavBar css.
    
    import NavBarWeather to NavBar

--- airport-municipal-anderson/src/components/NavBar/NavBar.js
+++ airport-municipal-anderson/src/components/NavBar/NavBar.js
@@ -3,17 +3,21 @@ import logoimage from '../../assets/logo.jpeg'
 import HamburgerMenu from './HamburgerMenu';
 import classes from './NavBar.module.css';
 import SocialMedia from './SocialMedia';
+import NavBarWeather from './NavBarWeather';
 
 const NavBar = () => {
     return (
-    
-    <div className={classes.navbar}>
+    <>
+        <div className={classes.navbar}>
         <img className={classes.log} src={logoimage} alt="Anderson Municipal Airport" width="125" height="125"/>
+        <NavBarWeather className={classes.navBarWeather} />
         <div className={classes.menu}>
             <SocialMedia/>
             <HamburgerMenu/>
         </div>
-    </div>)
+    </div>
+    </>
+    )
 };
 
 export default NavBar;
--- airport-municipal-anderson/src/components/NavBar/NavBarWeather.js
+++ airport-municipal-anderson/src/components/NavBar/NavBarWeather.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import WeatherContext from '../WeatherDisplay/WeatherContext';
 
 const NavBarWeather = () => {
+    const weatherData = useContext(WeatherContext);
 
+    if (!weatherData || !weatherData.currentTempData) {
+        return <p style={{ color: 'white' }}>No Data</p>;
+    }
+
+    const currentTemperature = weatherData.currentTempData; 
+
+    return (
+        <div style={{ color: 'white' }}>
+            <p>Current Temp: {currentTemperature ? `${currentTemperature} F` : 'N/A'}</p>
+        </div>
+    );
 };
 
 export default NavBarWeather;
-
