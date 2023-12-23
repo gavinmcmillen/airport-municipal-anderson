49e6bfba468212514167faeea59753a9c21ba56c
gavinmcmillen <gavinmcmillen@gmail.com>
2023-12-23 12:38:11 -0500

    Create doppler radar gif component.
    Implment component into App.js.
    
    Delete DopplerradarAPI and DopplerContext

---
+++ airport-municipal-anderson/src/API/DopplerRadar.js
@@ -0,0 +1,9 @@
+
+
+const DopplerRadar = () => {
+    return (
+        <img src='https://radar.weather.gov/ridge/standard/CENTGRLAKES_loop.gif' alt='NWS Central Radar'></img>
+    )
+}
+
+export default DopplerRadar;
--- airport-municipal-anderson/src/App.js
+++ airport-municipal-anderson/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
+import DopplerRadar from './API/DopplerRadar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <DopplerRadar/>
          <p>
           DopplerRadar Test
         </p>
